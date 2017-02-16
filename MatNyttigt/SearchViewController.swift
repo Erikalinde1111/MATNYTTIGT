b129edb8308cc6f32ea2d837bf98b97ab01bdf49
Erika Linde <erika.j.linde@gmail.com>
2017-02-16 22:35:15 +0100

    Lagt till ViewControllers för SearchView och för ListView

---
+++ MatNyttigt/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  MatNyttigt
+//
+//  Created by Erika Linde on 16/02/17.
+//  Copyright © 2017 Erika Linde. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: : UIViewController {
+
+}
---
+++ SearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchViewController.swift
+//  MatNyttigt
+//
+//  Created by Erika Linde on 16/02/17.
+//  Copyright © 2017 Erika Linde. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func printingMessage(_ sender: UIButton) {
+        print("osidogsd")
+    }
+   }
